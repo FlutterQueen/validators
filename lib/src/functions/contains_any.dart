08dd03a7508e80d472d130a45953c042555679f4
Ahmed Masoud <47630729+maxzod@users.noreply.github.com>
2021-04-03 07:38:58 +0200

    format

--- lib/src/functions/contains_any.dart
+++ lib/src/functions/contains_any.dart
@@ -1,4 +1,5 @@
 // check if the provider string contains any keywords form the provided list
 bool containsAny(String v, List<String> list) {
-  return list.any((e) => v.trim().toLowerCase().contains(e.trim().toLowerCase()));
+  return list
+      .any((e) => v.trim().toLowerCase().contains(e.trim().toLowerCase()));
 }
