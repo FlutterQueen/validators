c9098da8e76c2465bb611ff9df91332e6d96bfc4
Ahmed Masoud <maxzod66@gmail.com>
2021-09-27 00:55:33 +0200

    update

--- lib/src/rules/languages/is_arabic_chars.dart
+++ lib/src/rules/languages/is_arabic_chars.dart
@@ -1,2 +1,33 @@
 // TODO :: isArabic Chars
-bool isArabicChars(String input) => true;
+import 'package:queen_validators/queen_validators.dart';
+
+bool isArabicChars(String input) => containsAny(
+      input,
+      [
+        'ا',
+        'ب',
+        'ت',
+        'ث',
+        'ج',
+        'ح',
+        'خ',
+        'د',
+        'ز',
+        'ر',
+        '',
+        '',
+        '',
+        '',
+        '',
+        '',
+        '',
+        '',
+        '',
+        '',
+        '',
+        '',
+        '',
+        '',
+        '',
+      ],
+    );
