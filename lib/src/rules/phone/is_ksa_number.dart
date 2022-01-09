f566fafc9283ebc8391895ecf2ffb13fc65be4f8
MohamedRahman <MAbdelrahman@Worldofss.com>
2022-01-10 01:12:52 +0200

    format

--- lib/src/rules/phone/is_ksa_number.dart
+++ lib/src/rules/phone/is_ksa_number.dart
@@ -7,9 +7,10 @@ class ISKsaPhone extends TextValidationRule {
   @override
   bool isValid(String input) => isKsaPhone(input);
   @override
-  String get localizedError  => 'validation.is_ksa_num'.tr;
+  String get localizedError => 'validation.is_ksa_num'.tr;
 }
 
 bool isKsaPhone(String input) {
-  return RegExp(r'^(009665|9665|\+9665|05|5)(5|0|3|6|4|9|1|8|7)([0-9]{7})$').hasMatch(input);
+  return RegExp(r'^(009665|9665|\+9665|05|5)(5|0|3|6|4|9|1|8|7)([0-9]{7})$')
+      .hasMatch(input);
 }
